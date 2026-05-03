4ca61c9be3e6242cf6163fbcd6f4ff9123c1d42f
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-03 15:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def home():
-    return "✅ CMS is running on Azure!"
+    return "CMS is running on Azure!"
 
-if __name__ == "__main__":
+# Create article page
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('author')
+        body = request.form.get('body')
+
+        return f"Article Created: {title} by {author}"
+
+    return render_template("create.html")
+
+if __name__ == '__main__':
     app.run()
