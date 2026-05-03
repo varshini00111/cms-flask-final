3797b01a55f47a6bb9aba8e00a12d5a93128f1d6
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-03 15:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import pyodbc
 
-conn_str = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=your_server.database.windows.net;DATABASE=your_db;UID=your_user;PWD=your_password"
+import pyodbc
 
+conn_str = (
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=cms-server-12345.database.windows.net;"
+    "DATABASE=cms;"
+    "UID=cmsadmin;"
+    "PWD=CMS4dmin;"
+)
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
@@ -22,4 +29,25 @@ def create():
 
         return "✅ Article Saved!"
 
+    return render_template("create.html")
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        body = request.form['body']
+
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO articles (title, author, body) VALUES (?, ?, ?)",
+            (title, author, body)
+        )
+
+        conn.commit()
+        conn.close()
+
+return redirect('/')
     return render_template("create.html")
