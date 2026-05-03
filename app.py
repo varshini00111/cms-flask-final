cc41b2ce3481134d6f9b5dae74ef973e76ff562c
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-03 14:13:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ CMS is running on Azure!"
+
+@app.route('/login')
+def login():
+    return "Login Page"
+
+@app.route('/create')
+def create():
+    return "Create Article Page"
+
+# Azure needs this
+if __name__ == "__main__":
+    app.run()
